0c8e44bf82f99fb4a673ca363516796a53af0cbc
ServoCorp <80141504+ServoCorp@users.noreply.github.com>
2022-01-15 16:22:47 -0600

    date & time logic added

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,12 +4,29 @@ function printTime() {
     var h = d.getHours();
     var m = d.getMinutes();
     var s = d.getSeconds();
-    var ampm = h >= 12 ? 'pm' : 'am';
+    var ampm = h >= 12 ? 'PM' : 'AM';
     h = h % 12;
     h = h ? h : 12;
     m = m < 10 ? '0' + m : m;
     s = s < 10 ? '0' + s : s;
     var time = h + ':' + m + ':' + s + ' ' + ampm;
-    document.getElementById("time").innerHTML = time;
+    console.log(time);
+    document.getElementById('time').innerHTML = time;
     setTimeout(printTime, 1000);
     }
+
+function getDate() {
+    //get the current date and print it in the format of mm/dd/yyyy
+    var d = new Date();
+    var month = d.getMonth() + 1;
+    var day = d.getDate();
+    var year = d.getFullYear();
+    var date = month + '/' + day + '/' + year;
+    console.log(date);
+    document.getElementById('date').innerHTML = date;
+    }
+
+
+    // call the functions
+    printTime();
+    getDate();
